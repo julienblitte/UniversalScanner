6ea0128ebfdf3fee4f040ea2e077ebcee020e697
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2020-05-27 00:52:32 +0300

    Create scanner grpc service

---
+++ UniversalScannerService/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UniversalScannerService
+{
+    public class Startup
+    {
+        private static readonly string AllowAnyOriginPolicy = "AllowAnyOriginPolicy";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(option =>
+            {
+                option.AddPolicy(AllowAnyOriginPolicy, policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            services.AddGrpc();
+            services.AddSingleton(new UScannerServiceImpl(null));
+            services.AddGrpcReflection();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors(AllowAnyOriginPolicy);
+            app.UseGrpcWeb();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<UScannerServiceImpl>()
+                              .EnableGrpcWeb()
+                              .RequireCors(AllowAnyOriginPolicy);
+
+                endpoints.MapGrpcReflectionService();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
