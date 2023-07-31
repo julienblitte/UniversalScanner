fb7ae672dad96c2aedf377e21002a19e83a8df37
Julien Blitte <julien.blitte@gmail.com>
2023-07-31 18:26:30 +0200

    Fixed release script generator

--- UniversalScanner/Package_release.bat
+++ UniversalScanner/Package_release.bat
@@ -1,9 +1,4 @@
 @echo off
-
-cd bin\Release
-
 "%ProgramFiles(x86)%\Inno Setup 6\Compil32.exe" /cc "UniversalScanner.iss"
-
-powershell "Compress-Archive 'UniversalScanner.exe' UniversalScanner.Installer.%date%.zip"
-
-powershell "Compress-Archive 'UniversalScanner Setup.exe' UniversalScanner.Portable.%date%.zip"
+powershell "Compress-Archive 'UniversalScanner.exe' UniversalScanner.Portable.%date%.zip"
+powershell "Compress-Archive 'UniversalScanner Setup.exe' UniversalScanner.Installer.%date%.zip"
